130066937903bce64f2b6dcbc90d97fd55aae777
MDMoore1764 <mdmoore1764@gmail.com>
2025-07-12 02:27:13 -0400

    Add color pcker!

--- controller/lib/screens/control.dart
+++ controller/lib/screens/control.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
-class Control extends StatelessWidget {
+class Control extends StatefulWidget {
   const Control({super.key});
+
+  @override
+  State<Control> createState() => _ControlState();
+}
+
+class _ControlState extends State<Control> {
+  Color currentColor = Colors.blue;
+
+  void changeColor(Color color) => setState(() => currentColor = color);
+
+  void sendPattern(String patternName) {
+    debugPrint('Sending pattern: $patternName');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[const Text('control!')],
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Card(
+          elevation: 4,
+          child: ColorPicker(
+            pickerColor: currentColor,
+            onColorChanged: (color) => setState(() => currentColor = color),
+            paletteType: PaletteType.hueWheel,
+            enableAlpha: false,
+            showLabel: false,
+            portraitOnly: true,
+            displayThumbColor: false,
+            pickerAreaHeightPercent: 1.0,
+          ),
+        ),
+        const SizedBox(height: 20),
+      ],
     );
   }
 }
